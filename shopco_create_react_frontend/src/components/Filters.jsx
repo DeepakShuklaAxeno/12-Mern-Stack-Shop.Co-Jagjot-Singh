0963d9d2d2c593442f5619704323c8c2fc9b33df
DeepakShuklaAxeno <deepak.shukla@axeno.co>
2026-09-10 14:15:10 +0530

    removed merge conflict marker

--- shopco_create_react_frontend/src/components/Filters.jsx
+++ shopco_create_react_frontend/src/components/Filters.jsx
@@ -73,11 +73,9 @@ export default function Filters({
                 <div className="mt-2 space-y-4 font-sans text-base text-black/60">
                     {categories.map((category) => (
                         <button
-<<<<<<< HEAD
+
                             className="flex w-full justify-between transition-all duration-200 hover:brightness-110 hover:-translate-y-0.5 active:translate-y-0 active:brightness-95"
-=======
-                            className="flex w-full justify-between transition-all duration-200 hover:brightness-110 hover:-translate-y-0.5 active:translate-y-0 active:brightness-95 shadow-sm hover:shadow-md"
->>>>>>> d6f3545c1d815cd491b600394a9965c33eb6c70f
+
                             key={category._id}
                             onClick={() =>
                                 update("category", category._id)
